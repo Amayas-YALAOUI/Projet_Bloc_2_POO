b71760899645a3cac11d9e3a43ad0eefc58ea12e
Yanis LM <yanis.lieymahjoub@viacesi.fr>
2025-12-03 14:26:59 +0100

    classe en travaux

---
+++ App.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#pragma once
+#include "Jeu.h"
+
+namespace jdv{
+    class App{
+        public:
+            App();
+            void Choix_Etat(str);
+            ~App();
+    }
+
+}
