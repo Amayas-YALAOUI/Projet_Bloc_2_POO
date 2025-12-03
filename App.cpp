30a9080d3828838527681a0a3a422fd9097705c8
Yanis LM <yanis.lieymahjoub@viacesi.fr>
2025-12-03 15:35:08 +0100

    classe en travaux

--- App.cpp
+++ App.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+#include "App.h"
+#include <String>
+
+namespace jdv{
+    App::App(){
+        
+    }
+
+    App::void Choix_Etat(string){
+
+    }    
+    App::~App(){
+        
+    }
+}
--- App.h
+++ App.h
@@ -9,5 +9,4 @@ namespace jdv{
             void Choix_Etat(str);
             ~App();
     }
-
 }
