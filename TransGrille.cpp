0d40d4561593e387eca2ae9dc4ed46d769d75623
Normand Victor <normand.victor064@gmail.com>
2025-12-04 10:55:06 +0100

    Transcellule second jet

--- TransGrille.cpp
+++ TransGrille.cpp
@@ -28,4 +28,23 @@ Grille TransGrille::Trans_int_cel(std::vector<std::vector<int>> grille_int){
     }
     return grille_cel;
 }
+
+std::vector<std::vector<int>> Trans_cel_int(Grille grille){
+    x = grille.get_x();
+    y = grille.get_y();
+    grille_int = std::vector<std::vector<int>>;
+    for (int i = 0; i<x; i++){
+        for (int j = 0; j<y; j++){
+            if (grille_int[i][j] == 1){
+                *Vivant new_cel_viv = Vivant(i, j);
+                grille_int[i][j] = new_cel_viv;
+            }
+            if (grille_int[i][j] == 0){
+                *Mort new_cel_mor = Mort(i, j);
+                grille_int[i][j].set_cellule(new_cel_mor);
+            }
+        }
+    }
+    return grille_int;
+};
 }
